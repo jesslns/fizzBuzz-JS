78c05ee9343499cf8491e2ffbdb0e08bdd296c02
jesslns <jesslns@yahoo.com.hk>
2019-02-04 17:10:37 +0000

    Fizzbuzz class past all 6 tests

--- jasmine-standalone-3.3.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.3.0/spec/fizzBuzzSpec.js
@@ -2,10 +2,29 @@ describe("Fizzbuzz", function() {
 
     var fizzbuzz;
 
+    beforeEach(function() {
+       fizzbuzz = new Fizzbuzz();
+     });
+
   describe("knows when a number is", function() {
     it('divisible by 3', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
     });
   });
+
+  describe("knows when a number is", function() {
+    it('divisible by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+      expect(fizzbuzz.isDivisibleByFive(4)).toBe(false);
+    });
+  });
+
+  describe("knows when a number is", function() {
+    it('not divisible by s nor by 5', function() {
+      expect(fizzbuzz.otherNumber(4)).toBe(4);
+      expect(fizzbuzz.otherNumber(6)).toBe(6);
+    });
+  });
+
 });
--- jasmine-standalone-3.3.0/src/fizzBuzz.js
+++ jasmine-standalone-3.3.0/src/fizzBuzz.js
@@ -1,5 +1,13 @@
 var Fizzbuzz = function() {};
 
 Fizzbuzz.prototype.isDivisibleByThree = function (number) {
-  return true;
+  return (number%3 === 0);
 };
+
+Fizzbuzz.prototype.isDivisibleByFive = function (number) {
+  return (number%5 ===0);
+}
+
+Fizzbuzz.prototype.otherNumber = function (number) {
+  return number;
+}
